f742322f3383d7a6268a0047138e1a46faf5c930
Shashi230620 <shashi.bhatt2306@gmail.com>
2024-11-13 22:14:50 +0530

    comment update

--- frontent/src/index.js
+++ frontent/src/index.js
@@ -7,7 +7,6 @@ import { BrowserRouter } from 'react-router-dom';
 import Player from "./Website/Player";
 import App from './App';
 import "./Website/Website.css";
-import MovieDetails from './Website/MovieDetails';
 import CommentBox from './Website/CommentBox';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
